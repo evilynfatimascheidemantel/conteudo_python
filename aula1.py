0d4f3f38001c7c5b043caeb221bd4028d5e5d0db
Aluno SENAI <Aluno@SN-388027.edu.fiesc.com.br>
2025-10-02 11:28:12 -0300

    Explicação comando print

---
+++ aula1.py
@@ -0,0 +1,12 @@
+print("Olá mundo!")
+# Escapei da maldição
+
+print("Eu sou a Evilyn",end="\n")
+print("E","eu", "sou", "Hacker",sep=" ",end="\n")
+
+print("Lógica de programação")
+print("lógica","de", "programação",sep="_",end="\n")
+print("lógica de programação",end="SENAI\n")
+print("lógica",end=" ")
+print("de",end=" ")
+print("programação",end=" ")
