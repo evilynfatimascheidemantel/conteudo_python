89b80c6d48ef5e69170f5bc9d77315c86138d37c
Aluno SENAI <Aluno@SN-388027.edu.fiesc.com.br>
2025-10-03 09:48:25 -0300

    aula 1: comando print

--- aula1.py
+++ aula1.py
@@ -10,3 +10,18 @@ print("lógica de programação",end="SENAI\n")
 print("lógica",end=" ")
 print("de",end=" ")
 print("programação",end=" ")
+
+# isso é uma comentário
+# vamos escapar da maldição
+
+# Esse é o comando print,
+# eleserve para imprimir na tela
+print("olá mundo!")
+
+# podemos mudar o comportamento da finalização
+# do comando com o parâmetro end
+print("esse é o curso de:",end=" ")
+print("lógica","de","programação")
+# Podemos mudar o comprotamendo do separador
+# com o parâmetro sep
+print("frase","com","hifen",sep="-")
